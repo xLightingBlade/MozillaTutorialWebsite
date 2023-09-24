efe6a054fca2bec687e8d478c87cd685dc68cf1b
Gabriele Nigro <127190728+xLightingBlade@users.noreply.github.com>
2023-09-25 01:10:01 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/Gurren_Lagann_key_visual.jpg") {
+    myImage.setAttribute("src", "images/gurrenTitle.png");
+  } else {
+    myImage.setAttribute("src", "images/Gurren_Lagann_key_visual.jpg");
+  }
+};
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+  function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
+  
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+};
+
+  
+  
